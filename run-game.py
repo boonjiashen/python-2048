829f373b250ce6b80ca7646df0e7f4a1cb68d82d
Jia Shen Boon <jiashenb@amazon.com>
2019-09-24 19:42:55 +0900

    Add swipe-left transform

--- run-game.py
+++ run-game.py
@@ -27,10 +27,12 @@ class Board(tuple):
     HEIGHT = WIDTH = 4
 
     def __new__(cls, x: Tuple[Tuple[Tile]]):
+        """TODO: make not require tuple grid"""
         if len(x) != cls.HEIGHT:
-            raise Exception(f"Height needs to be {cls.HEIGHT} but is {len(x)}")
-        if any(len(row) != cls.WIDTH for row in x):
-            raise Exception(f"Width not ${cls.WIDTH}")
+            raise Exception(f"Height needs to be {cls.HEIGHT} but is {len(x)}.")
+        for row in x:
+            if len(row) != cls.WIDTH:
+                raise Exception(f"Width needs to be {cls.WIDTH} but is {len(row)}.")
         return super().__new__(cls, x)
 
     def __str__(self):
@@ -61,15 +63,36 @@ class SpawnerTransformation(Transformation):
 
         return Board(tmp_board)
 
+
+class LeftTransform(Transformation):
+    """User presses left button"""
+
+    def transform(self, board: Board) -> Board:
+        def transform_row(row):
+            nonblank_tiles = [x for x in row if x != BLANK_TILE]
+            i, new_row = 0, []
+            while i < len(nonblank_tiles):
+                curr_tile = nonblank_tiles[i]
+                to_merge = i != len(nonblank_tiles) - 1 and curr_tile == nonblank_tiles[i+1]
+                if to_merge:
+                    new_row.append(TilePower(curr_tile + 1))
+                    i += 2
+                else:
+                    new_row.append(curr_tile)
+                    i += 1
+            new_row += [BLANK_TILE] * (len(row) - len(new_row))
+            return new_row
+        return Board([transform_row(row) for row in board])
+
+
+leftTransform = LeftTransform()
+
 EMPTY_BOARD = Board([[BLANK_TILE] * Board.HEIGHT] * Board.WIDTH)
-row = (TilePower(1), BLANK_TILE, TilePower(3), TilePower(4))
+row = (TilePower(1), TilePower(1), TilePower(3), TilePower(4))
 some_board = Board([row] * 4)
 
 spawnerTrans = SpawnerTransformation()
 
 if __name__ == "__main__":
     curr_board = EMPTY_BOARD
-    print(curr_board, "\n")
-    for _ in range(5):
-        curr_board = spawnerTrans.transform(curr_board)
-        print(curr_board, "\n")
+    print(leftTransform.transform(some_board))
